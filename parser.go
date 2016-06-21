075fdf07a3c151ec4d995d3d7630d940f8a10723
Darin Minamoto <darinm223@gmail.com>
2016-06-21 12:48:40 -0700

    Started parseNumOrRange and parseString implementations

--- parser.go
+++ parser.go
@@ -10,6 +10,7 @@ var (
 	InvalidChErr           = errors.New("Invalid character scanned")
 	InvalidTokenErr        = errors.New("Invalid token scanned")
 	ExpectedStrErr         = errors.New("String different from expected")
+	NumRangeErr            = errors.New("Error parsing number or range")
 )
 
 type Token int
@@ -190,14 +191,63 @@ func (p *Parser) parseIf() (Expr, error) {
 	}, nil
 }
 
+// parseNumOrRange either returns a integer expression
+// or a range expression between two integers.
 func (p *Parser) parseNumOrRange() (Expr, error) {
-	// TODO(DarinM223): implement this
-	return nil, nil
+	isRange := false
+	num1, num2 := -1, -1
+	for {
+		ch := p.text[p.pos]
+		switch {
+		case '0' <= ch && ch <= '9':
+			if isRange {
+				if num2 == -1 {
+					num2 = 0
+				}
+				num2 *= 10
+				num2 += int(ch) - int('0')
+			} else {
+				if num1 == -1 {
+					num1 = 0
+				}
+				num1 *= 10
+				num1 += int(ch) - int('0')
+			}
+			p.pos++
+		case ch == '.':
+			isRange = true
+			p.pos++
+			p.expectString(".")
+		case ch == ' ' || ch == '\t' || ch == '\n':
+			break
+		default:
+			return nil, NumRangeErr
+		}
+	}
+	if isRange && num1 != -1 && num2 != -1 {
+		return RangeExpr{IntExpr{num1}, IntExpr{num2}}, nil
+	} else if !isRange && num1 != 1 {
+		return IntExpr{num1}, nil
+	}
+	return nil, NumRangeErr
 }
 
+// parseString parses a string from the file.
 func (p *Parser) parseString() (Expr, error) {
-	// TODO(DarinM223): implement this
-	return nil, nil
+	p.expectString("\"")
+	var str bytes.Buffer
+	for {
+		ch := p.text[p.pos]
+		switch {
+		case ch == '"':
+			return StringExpr{str.String()}, nil
+		default:
+			if err := str.WriteByte(ch); err != nil {
+				return StringExpr{}, err
+			}
+			p.pos++
+		}
+	}
 }
 
 func (p *Parser) parseBlock() (Stmt, error) {
@@ -228,6 +278,11 @@ func (p *Parser) parseExpr() (Expr, error) {
 	case ch == '"':
 		return p.parseString()
 	}
+	return nil, nil
+}
+
+func (p *Parser) parseStmt() (Stmt, error) {
+	// TODO(DarinM223): implement this
 	return nil, nil
 }
 
