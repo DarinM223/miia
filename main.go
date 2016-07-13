c539fef91438a477162a956e2e0e27a7d54dea89
Darin Minamoto <darinm223@gmail.com>
2016-07-13 11:58:50 -0700

    Implemented main function to parse and compile file

--- main.go
+++ main.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/DarinM223/http-scraper/graph"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	if len(os.Args) <= 1 {
+		fmt.Println("Need to specify path to code file")
+		return
+	}
+
+	path := os.Args[1]
+	absPath, err := filepath.Abs(path)
+	if err != nil {
+		fmt.Println("Error translating to absolute path: ", err)
+		return
+	}
+
+	code, err := ioutil.ReadFile(absPath)
+	if err != nil {
+		fmt.Println("Error reading file: ", err)
+		return
+	}
+
+	parser := Parser{0, string(code)}
+	globals := graph.NewGlobals()
+
+	expr, err := parser.parseExpr()
+	if err != nil {
+		fmt.Println("Error parsing expression: ", err)
+		return
+	}
+
+	resultNode, err := CompileExpr(globals, expr, NewScope(nil))
+	if err != nil {
+		fmt.Println("Error compiling expression: ", err)
+		return
+	}
+
+	resultChan := make(chan graph.Msg, graph.InChanSize)
+	resultNode.ParentChans()[69] = resultChan
+
+	globals.Run()
+
+	if msg, ok := <-resultChan; ok {
+		fmt.Println("Result: ", msg)
+	}
 }
